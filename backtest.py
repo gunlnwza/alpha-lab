244bc63d8b3a3afdc06ad612149453f7841ecfe3
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-02-25 14:12:47 +0700

    refactor: train.py, backtest.py

--- backtest.py
+++ backtest.py
@@ -7,100 +7,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from utils import load_parquet, drop_weekend
-from train import get_features_target
+from features import get_inputs
 
-path = Path("models", "logreg_v1.pkl")
-clf = joblib.load(path)
 
-# --- Load your dataset (adjust path as needed)
-def backtest_one(symbol):
-    # data = load_parquet("twelve_data", symbol, "5min")
-    # data = load_parquet("twelve_data", symbol, "1hour")
-    try:
-        data = load_parquet("twelve_data", symbol, "1day")
-    except FileNotFoundError:
-        return
-    # data = 1 / data
-    df = drop_weekend(data)
+def load_model(name: str):
+    path = Path("models", f"{name}.pkl")
+    clf = joblib.load(path)
+    return clf
 
-    # --- Feature preparation (must match training pipeline)
-    X, _ = get_features_target(df)
 
-    # --- Predict
-    pred = clf.predict(X)
+def load_data(symbol: str):
+    df = load_parquet("twelve_data", symbol, "5min")
+    df = drop_weekend(df)
+    return df
 
-    close = df["close"].to_numpy()
 
-    # atr = ta.atr(df["high"], df["low"], df["close"], length=14).to_numpy()
-    std = ta.stdev(df["close"], 14).to_numpy()
-    vol = std
-
-    ema_short = ta.ema(df["close"], 100).to_numpy()
-    ema_long = ta.ema(df["close"], 200).to_numpy()
-
-    entry_idx = []
-    exit_idx = []
-
-    position = 0
-    entry_price = 0.0
-    pnl = 0.0
-    trailing_stop = 0.0
-
+class Record:
     SL_VOL_MUL = 10
-    for i in range(len(pred)):
 
-        # Enter long
-        indicators = not (np.isnan(vol[i]) or np.isnan(ema_short[i]) or np.isnan(ema_long[i]))
-        if position == 0 and pred[i] == 1 and indicators and ema_short[i] > ema_long[i]:
-            position = 1
-            entry_price = close[i]
-            trailing_stop = close[i] - SL_VOL_MUL * vol[i]  # 1 ATR initial stop
-            entry_idx.append(i)
+    def __init__(self, symbol: str, df: pd.DataFrame):
+        self.symbol = symbol.upper()
+        self.close = df["close"].to_numpy()
 
-        # Manage open position with trailing stop
-        elif position == 1:
+        # account / orders
+        self.entry_idx = []
+        self.exit_idx = []
+        self.position = 0
+        self.entry_price = 0.0
+        self.pnl = 0.0
+        self.trailing_stop = 0.0
 
-            # Update trailing stop (only move upward)
-            if not np.isnan(vol[i]):
-                new_stop = close[i] - SL_VOL_MUL * vol[i]
-                trailing_stop = max(trailing_stop, new_stop)
+        # indicators
+        self.vol = ta.atr(df["high"], df["low"], df["close"], length=14).to_numpy()
+        self.ma_short = ta.linreg(df["close"], 20).to_numpy()
+        self.ma_long = ta.kama(df["close"], 50).to_numpy()
 
-            # Exit if price hits trailing stop
-            if close[i] <= trailing_stop:
-                pnl += close[i] - entry_price
-                position = 0
-                exit_idx.append(i)
+    def all_notna(self, i: int):
+        return not (np.isnan(self.vol[i]) or np.isnan(self.ma_short[i]) or np.isnan(self.ma_long[i]))
 
-    # Close any open position at final price
-    if position == 1:
-        pnl += close[-1] - entry_price
+    def update_trailing_stop(self, i: int):
+        if not np.isnan(self.vol[i]):
+            new_stop = self.close[i] - self.SL_VOL_MUL * self.vol[i]
+            self.trailing_stop = max(self.trailing_stop, new_stop)
 
-    print(f"{s.upper()} | Final PnL: {pnl:.4f}")
+    def exit_if_hit_stop(self, i: int):
+        if self.close[i] <= self.trailing_stop:
+            self.pnl += self.trailing_stop - self.entry_price
+            self.position = 0
+            self.exit_idx.append(i)
 
-    # --- Visualization
+    def enter_long(self, i: int):
+        self.position = 1
+        self.entry_price = self.close[i]
+        self.trailing_stop = self.close[i] - self.SL_VOL_MUL * self.vol[i]
+        self.entry_idx.append(i)
+
+
+def report(rec: Record):
+    print(f"{rec.symbol} | Final PnL: {rec.pnl:.4f}")
+
+
+def visualize(rec: Record):
     plt.figure(figsize=(14, 6))
+    plt.plot(rec.close, label="Close Price", linewidth=1)
 
-    plt.plot(close, label="Close Price", linewidth=1)
-    plt.plot(ema_short, linewidth=1)
-    plt.plot(ema_long, linewidth=1)
-    # plt.plot(close - SL_VOL_MUL * vol, label="SL", linewidth=1)
-
-    # Plot entries
-    if entry_idx:
-        plt.scatter(entry_idx, close[np.array(entry_idx)],
+    if rec.entry_idx:  # Plot entries
+        plt.scatter(rec.entry_idx, rec.close[np.array(rec.entry_idx)],
                     marker="^", color="green", s=80, label="Entry")
-
-    # Plot exits
-    if exit_idx:
-        plt.scatter(exit_idx, close[np.array(exit_idx)],
+    if rec.exit_idx:  # Plot exits
+        plt.scatter(rec.exit_idx, rec.close[np.array(rec.exit_idx)],
                     marker="v", color="red", s=80, label="Exit")
 
-    plt.title(f"Backtest Visualization {s.upper()}")
+    plt.title(f"Backtest Visualization {rec.symbol.upper()}")
     plt.legend()
     plt.grid(alpha=0.3)
     plt.tight_layout()
     plt.show()
 
 
-for s in ["xauusd", "usdcad", "audusd", "eurusd", "usdjpy"]:
-    backtest_one(s)
+def backtest_one(symbol: str):
+    clf = load_model("logreg_v1")
+    df = load_data(symbol)
+
+    rec = Record(symbol, df)
+
+    X = get_inputs(df)
+    pred = clf.predict(X)
+    for i in range(len(pred)):
+        if rec.position == 0:
+            if pred[i] == 1 and rec.all_notna(i) and rec.ma_short[i] > rec.ma_long[i]:  # Enter long
+                rec.enter_long(i)
+        elif rec.position == 1:  # Manage open position with trailing stop
+            rec.update_trailing_stop(i)
+            rec.exit_if_hit_stop(i)
+
+    if rec.position == 1:  # Close any open position at final price
+        pnl += rec.close[-1] - rec.entry_price
+
+    report(rec)
+    visualize(rec)
+
+
+def main():
+    symbols = ["xauusd"]
+    # symbols = ["xauusd", "usdcad", "audusd", "eurusd", "usdjpy"]
+
+    for s in symbols:
+        backtest_one(s)
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import joblib
+import sys
 
 import pandas as pd
 import pandas_ta as ta
-import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -11,93 +11,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 
-from utils import load_parquet, drop_weekend, get_chunks, triple_barrier_labels
+from utils import load_parquet, drop_weekend, divide_timeseries, triple_barrier_labels
+from features import get_inputs, get_inputs_labels
 
 
-def plot_labels(df: pd.DataFrame, signals: pd.Series):
-    s = signals
-
-    # Show stats
-    n = len(s)
-    tp_hit = s[s == 1].count()
-    time_limit = s[s == 0].count()
-    sl_hit = s[s == -1].count()
-    print(f"{'':10} {'count':>5} {'percent':>7}")
-    print(f"{'tp_hit':10} {tp_hit:5} {tp_hit / n:7.2f}")
-    print(f"{'sl_hit':10} {sl_hit:5} {sl_hit / n:7.2f}")
-    print(f"{'time_limit':10} {time_limit:5} {time_limit / n:7.2f}")
-    print(f"{'total':10} {n:5} {1:7.2f}")
-
-    # Ploting
+def plot_buy_labels(df: pd.DataFrame, signals: pd.Series):
     plt.figure(figsize=(15, 5))
-    plt.title("Triple Barrier Labels")
+    plt.title("Buy Labels")
 
-    plt.plot(df.close,
+    plt.plot(
+        df.close,
         color="black",
         marker='o', markersize=1,
-        linestyle=':', linewidth=0.5)
+        linestyle=':', linewidth=0.5
+    )
 
-    # Buy label
-    plt.scatter(df.index[s == 1], df.close[s == 1],
-        color="green", s=15)
-
-    # Sell label
-    plt.scatter(df.index[s == -1], df.close[s == -1],
-        color="red", s=15)
+    plt.scatter(
+        df.index[signals == 1],
+        df.close[signals == 1],
+        color="green",
+        s=15
+    )
 
     plt.show()
-
-
-def get_features_target(df: pd.DataFrame, signals: pd.Series = None):
-    """
-    Feature engineer `df`, and trim `signals` so there's no NaN.
-
-    - Return only `X` if only df is provided.
-    - Return both `X` and `y` if `signals` is also provided.
-    """
-    
-    # TODO: use Kalman filter to smooth
-    df = df.copy()
-
-    df["smoothed_price"] = df.ta.sma(5)
-    df["short"] = df.ta.ema(20)
-    df["long"] = df.ta.ema(50)
-
-    df["rsi"] = df.ta.rsi(20)
-
-    df["smoothed_price/short"] = df["smoothed_price"] / df["short"]  # too much noise
-    df["smoothed_smoothed_price/short"] = df["smoothed_price/short"].rolling(5).mean()
-
-    df["smoothed_price/long"] = df["smoothed_price"] / df["long"]  # too much noise
-    df["smoothed_smoothed_price/long"] = df["smoothed_price/long"].rolling(5).mean()
-    
-    df["short/long"] = df["short"] / df["long"]
-
-    # boolean
-    df["smoothed_price_above_short"] = (df["smoothed_price/short"] > 1).astype("int8")
-    df["smoothed_price_above_long"]  = (df["smoothed_price/long"] > 1).astype("int8")
-    df["short_above_long"]  = (df["short"] > df["long"]).astype("int8")
-    df["rsi_overbought"] = (df["rsi"] > 70).astype("int8")
-    df["rsi_oversold"] = (df["rsi"] < 30).astype("int8")
-
-    cols = [
-        "smoothed_smoothed_price/short",
-        "smoothed_smoothed_price/long",
-        "short/long",
-        "rsi",
-
-        "smoothed_price_above_short",
-        "smoothed_price_above_long",
-        "short_above_long",
-        "rsi_overbought",
-        "rsi_oversold"
-    ]
-    X = df[cols].dropna()
-    if signals is None:
-        return X, None
-
-    y = signals.reindex(X.index).astype("int8")  # safe alignment
-    return X, y
 
 
 def validate_train_result(X_test, y_test, clf, name):
@@ -108,10 +44,11 @@ def validate_train_result(X_test, y_test, clf, name):
 
 
 def visual_backtest(df, clf, name):
-    X, _ = get_features_target(df)
+    X = get_inputs(df)
 
+    THRESHOLD = 0.5
     proba = clf.predict_proba(X)[:, 1]   # probability of class 1
-    y_pred = proba > 0.5                 # decision threshold
+    y_pred = proba > THRESHOLD
 
     X = X.copy()
     X["close"] = df["close"]
@@ -126,8 +63,8 @@ def visual_backtest(df, clf, name):
     # --- Price chart ---
     axes[0].set_title(name)
     axes[0].plot(X.index, X.close, label="Price", linewidth=1, color="black", alpha=0.5)
-    axes[0].plot(df.ta.sma(5)[X.index])
-    axes[0].plot(df.ta.ema(100)[X.index])
+    axes[0].plot(ta.kama(X["close"], 50))
+    axes[0].plot(ta.kama(X["close"], 100))
     axes[0].scatter(
         X.index[y_pred],
         X.close[y_pred],
@@ -139,7 +76,7 @@ def visual_backtest(df, clf, name):
 
     # --- Probability panel ---
     axes[1].plot(X.index, proba, color="blue", label="P(buy)")
-    axes[1].axhline(0.5, color="red", linestyle="--", alpha=0.5)
+    axes[1].axhline(THRESHOLD, color="red", linestyle="--", alpha=0.5)
     axes[1].set_ylim(0, 1)
     axes[1].legend()
 
@@ -149,14 +86,16 @@ def visual_backtest(df, clf, name):
 
 def main():
     xauusd_5min = load_parquet("twelve_data", "xauusd", "5min")
-    week_chunks = get_chunks(drop_weekend(xauusd_5min))
-
+    week_chunks = divide_timeseries(drop_weekend(xauusd_5min))
     df = week_chunks[0]
+
     signals = triple_barrier_labels(df, bars=36, tp_atr_mul=4, sl_atr_mul=1)
     signals = (signals == 1).astype("int8")
-    plot_labels(df, signals)
+    plot_buy_labels(df, signals)
+    if len(sys.argv) == 2:
+        sys.exit()
 
-    X, y = get_features_target(df, signals)  # Extract features and target
+    X, y = get_inputs_labels(df, signals)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle=False, random_state=42)
 
     lr_clf = LogisticRegression(random_state=42)
