6401b0394e51464c8fc1feea7eef899ffac074db
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-02-27 21:14:26 +0700

    fix: log_reg.py update

--- alpha_lab/strategies/log_reg.py
+++ alpha_lab/strategies/log_reg.py
@@ -1,19 +1,66 @@
 from alpha_lab.backtest.account import Account, Side, OrderType
 from alpha_lab.backtest.bot import BacktestBot, PrecomputedData
-from alpha_lab.utils import ForexData
+from alpha_lab.utils import ForexData, load_model
 
-from pathlib import Path
-import joblib
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
-
 
 GATE_MA_SHORT_PERIOD = 50
 GATE_MA_LONG_PERIOD = 200
 
 ATR_PERIOD = 10
 SL_VOL_MUL = 10
+
+
+def _compute_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Return raw `X`
+    - `df` is a copy of OHLCV dataframe
+    """
+
+    # indicator
+    df["smooth(price)"] = ta.linreg(df["close"], 20)
+    df["short"] = ta.ema(df["close"], 20)
+    df["long"] = ta.ema(df["close"], 50)
+    df["rsi"] = ta.rsi(df["close"], 20)
+
+    # numeric features
+    df["smooth(price)/short"] = df["smooth(price)"] / df["short"]  # too much noise, need smoothing
+    df["smooth(smooth(price)/short)"] = ta.linreg(df["smooth(price)/short"], 5)
+
+    df["smooth(price)/long"] = df["smooth(price)"] / df["long"]  # too much noise, need smoothing
+    df["smooth(smooth(price)/long)"] = ta.linreg(df["smooth(price)/long"], 5)
+
+    df["short/long"] = df["short"] / df["long"]
+
+    # boolean features
+    df["smooth(price)_above_short"] = (df["smooth(price)/short"] > 1).astype("int8")
+    df["smooth(price)_above_long"]  = (df["smooth(price)/long"] > 1).astype("int8")
+    df["short_above_long"]  = (df["short"] > df["long"]).astype("int8")
+    df["rsi_overbought"] = (df["rsi"] > 70).astype("int8")
+    df["rsi_oversold"] = (df["rsi"] < 30).astype("int8")
+
+    return df[[
+        # numeric
+        "smooth(smooth(price)/short)",
+        "smooth(smooth(price)/long)",
+        "short/long",
+        "rsi",
+
+        # boolean
+        "smooth(price)_above_short",
+        "smooth(price)_above_long",
+        "short_above_long",
+        "rsi_overbought",
+        "rsi_oversold"
+    ]]
+
+
+def get_features(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    X = _compute_features(df).dropna()
+    return X
 
 
 class LogRegBot(BacktestBot):
@@ -23,10 +70,11 @@ class LogRegBot(BacktestBot):
     def precompute_data(self, forex_data: ForexData) -> PrecomputedData:
         data = PrecomputedData(forex_data)
         
-        # Signals
+        data.vol = forex_data.ohlcv.ta.atr(ATR_PERIOD).to_numpy()
+
         X = get_features(forex_data.ohlcv)
-        low_tf_clf = joblib.load(Path("alpha_lab", "models", "artifacts", "logreg_v1.pkl"))
-        low_tf_raw = pd.Series(low_tf_clf.predict(X), index=X.index)
+        clf = load_model("logreg_v1")
+        low_tf_raw = pd.Series(clf.predict(X), index=X.index)
         low_tf = low_tf_raw.reindex(forex_data.ohlcv.index)
 
         ma_short = ta.ema(forex_data.ohlcv["close"], GATE_MA_SHORT_PERIOD)
@@ -37,25 +85,24 @@ class LogRegBot(BacktestBot):
         assert signals.index.to_list() == forex_data.ohlcv.index.to_list()
         data.signals = signals.to_numpy()
 
-        # Misc
-        data.misc["vol"] = forex_data.ohlcv.ta.atr(ATR_PERIOD).to_numpy()  # Additional data, for SL
-
         return data
     
     def calculate_sl(self, close: float, vol: float):
         return close - SL_VOL_MUL * vol
 
-    def act(self, idx: int, data: PrecomputedData, acc: Account):
+    def act(self, data: PrecomputedData, acc: Account):
         order = acc.get_order()
-        close = data.prices.close[idx]
-        vol = data.misc["vol"][idx]
+        bar = data.bar
+        now = data.now
+        signal = data.signals[now]
+        vol = data.vol[now]
 
         if order:
             if order.type == OrderType.POSITION:
-                new_sl = self.calculate_sl(close, vol)
+                new_sl = self.calculate_sl(bar.close, vol)
                 if new_sl > order.sl:
-                    order.set_sl(close, new_sl)
+                    order.set_sl(bar.close, new_sl)
         else:
-            if data.signals[idx] and not np.isnan(vol):
-                sl = self.calculate_sl(close, vol)
-                acc.open_order(Side.BUY, OrderType.POSITION, idx, close, sl)
+            if signal and not np.isnan(vol):
+                sl = self.calculate_sl(bar.close, vol)
+                acc.open_order(Side.BUY, OrderType.POSITION, now, bar.close, sl)
